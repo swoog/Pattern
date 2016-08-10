e23b76e422d6923bc448f98692b7d61c7d4ea415
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-08-10 21:07:15 +0200

    Implement binding to Ninject

--- src/Pattern.Core.Ninject/NinjectStandardKernel.cs
+++ src/Pattern.Core.Ninject/NinjectStandardKernel.cs
@@ -7,13 +7,23 @@ namespace Pattern.Core.Ninject
 {
     using global::Ninject;
 
-    public class NinjectStandardKernel
+    public class NinjectStandardKernel : Pattern.Core.Interfaces.IKernel
     {
-        private StandardKernel standardKernel;
+        private readonly StandardKernel standardKernel;
 
         public NinjectStandardKernel()
         {
             this.standardKernel = new StandardKernel();
+        }
+
+        public void Bind(Type @from, Type to)
+        {
+            this.standardKernel.Bind(from).To(to);
+        }
+
+        public object Get(Type @from)
+        {
+            return this.standardKernel.Get(@from);
         }
     }
 }
