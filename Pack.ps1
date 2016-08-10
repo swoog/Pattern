52e5c67fe19b7594dc3a7e350f567b7eb854e186
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-08-10 21:45:58 +0200

    Add release configuration

--- Pack.ps1
+++ Pack.ps1
@@ -6,5 +6,5 @@ $projects = Get-ChildItem -Path "src" -Recurse "Project.json"
 Write-Output $projects
 foreach($project in $projects){
 	Write-Output "dotnet pack --no-build $($project.FullName)"
-	dotnet pack --no-build $($project.FullName) --version-suffix $versionNumber
+	dotnet pack -c Release --no-build $($project.FullName) --version-suffix $versionNumber
 }
