d9366b19cc21c8eb4343681344dae0d9ac1523de
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-08-14 21:16:40 +0200

    Add Get<List<>> to get all instance of a binding

--- src/Pattern.Core/Kernel.cs
+++ src/Pattern.Core/Kernel.cs
@@ -2,20 +2,30 @@
 {
     using Interfaces;
     using System;
+    using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
     public class Kernel : IKernel
     {
-        private readonly Dictionary<Type, IFactory> binds;
+        private readonly Dictionary<Type, IList<IFactory>> binds;
 
         public Kernel()
         {
-            this.binds = new Dictionary<Type, IFactory>();
+            this.binds = new Dictionary<Type, IList<IFactory>>();
         }
 
         public void Bind(Type @from, IFactory toFactory)
         {
-            this.binds.Add(@from, toFactory);
+            if (!this.binds.ContainsKey(@from))
+            {
+                this.binds.Add(@from, new List<IFactory>() { toFactory });
+            }
+            else
+            {
+                this.binds[@from].Add(toFactory);
+            }
         }
 
         public object Get(Type parentType, Type @from)
@@ -26,14 +36,35 @@
                 return this;
             }
 
+            var any = @from.GetTypeInfo().GetInterfaces().FirstOrDefault(i => i.GetTypeInfo().IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+            if (any != null)
+            {
+                var list = @from.GetTypeInfo().GetConstructors().First().Invoke(null) as IList;
+
+                callContext = new CallContext(any.GenericTypeArguments[0], parentType);
+                var factories = this.GetFactories(callContext);
+                foreach (var factory in factories)
+                {
+                    list.Add(factory.Create());
+                }
+
+                return list;
+            }
+
+            var to = this.GetFactories(callContext);
+
+            return to.Select(t => t.Create()).First();
+        }
+
+        private IList<IFactory> GetFactories(CallContext callContext)
+        {
             if (!this.binds.ContainsKey(callContext.InstanciatedType))
             {
                 throw new InjectionException(callContext.InstanciatedType, callContext.Parent);
             }
 
             var to = this.binds[callContext.InstanciatedType];
-
-            return to.Create();
+            return to;
         }
     }
 }
---
+++ tests/Pattern.Core.Tests/InstanciateCollectionObject.cs
@@ -0,0 +1,43 @@
+﻿namespace Pattern.Core.Tests
+{
+    using System.Collections.Generic;
+
+    using Pattern.Core.Interfaces;
+    using Pattern.Core.Interfaces.Factories;
+    using Pattern.Core.Tests.Fakes;
+
+    using Xunit;
+
+    public class InstanciateCollectionObject
+    {
+        private IKernel kernel;
+
+        public InstanciateCollectionObject()
+        {
+            this.kernel = new Kernel();
+        }
+
+        [CustomFact(DisplayName = nameof(Should_instanciate_a_collection_When_bind_two_class_on_same_interface))]
+        public void Should_instanciate_a_collection_When_bind_two_class_on_same_interface()
+        {
+            this.kernel.Bind(typeof(ISimpleClass), this.GetTypeFactory<SimpleClass>());
+            this.kernel.Bind(typeof(ISimpleClass), this.GetTypeFactory<SimpleClass2>());
+
+            var collection = this.kernel.Get<List<ISimpleClass>>();
+
+            Assert.Equal(2, collection.Count);
+            Assert.IsType<SimpleClass>(collection[0]);
+            Assert.IsType<SimpleClass2>(collection[1]);
+        }
+        // Should throw error when bind two_class_on_same_interface
+
+        private TypeFactory GetTypeFactory<T>()
+        {
+            return new TypeFactory(typeof(T), this.kernel);
+        }
+    }
+
+    public class SimpleClass2 : ISimpleClass
+    {
+    }
+}
