c1d1458816415e9da1ed364f4ac5a9b59750112f
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-12-31 22:13:45 +0100

    Add RelayCommand

---
+++ tests/Pattern.Mvvm.Tests/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Pattern.Mvvm.Tests
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action action;
+        private Func<bool> canExecute;
+
+        public RelayCommand()
+        {
+        }
+
+        public RelayCommand(Action action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            bool? canExecute = this.canExecute?.Invoke();
+            return canExecute ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(this.CanExecute(parameter))
+            {
+                this.action?.Invoke();
+            }
+        }
+    }
+}
