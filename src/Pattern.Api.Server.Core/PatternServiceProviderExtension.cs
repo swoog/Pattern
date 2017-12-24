25722f5bff1281711f154a3a5bab4ae2a6e2999d
Aurélien GALTIER <aurelien.galtier@live.fr>
2017-12-24 09:34:18 +0100

    Add pattern API server

---
+++ src/Pattern.Api.Server.Core/PatternServiceProviderExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Pattern.Core.Interfaces;
+
+namespace Pattern.Api.Server.Core
+{
+    public static class PatternServiceProviderExtension
+    {
+        public static IServiceProvider ToServiceProvider(this IKernel kernel, IServiceCollection services)
+        {
+            return new PaternServiceProvider(kernel, services);
+        }
+    }
+}
---
+++ src/Pattern.Api.Server.Core/PatternServiceScope.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pattern.Api.Server.Core
+{
+    public class PatternServiceScope : Microsoft.Extensions.DependencyInjection.IServiceScopeFactory, IServiceScope
+    {
+        public PatternServiceScope(IServiceProvider serviceProvider)
+        {
+            this.ServiceProvider = serviceProvider;
+        }
+
+        public IServiceScope CreateScope()
+        {
+            return new PatternServiceScope(this.ServiceProvider);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IServiceProvider ServiceProvider { get; }
+    }
+}
