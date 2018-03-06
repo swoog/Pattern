27bbd2b56dedb32a4d71f8bd5e33d3a6b9a90c5f
Aurélien GALTIER <aurelien.galtier@live.fr>
2018-03-06 02:59:05 +0100

    Add logger for tracewritter

---
+++ src/Pattern.Logging.TraceWriter/TraceWriterLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pattern.Logging.TraceWriter
+{
+    public class TraceWriterLogger : ILogger
+    {
+        private readonly Microsoft.Azure.WebJobs.Host.TraceWriter log;
+
+        public TraceWriterLogger(Microsoft.Azure.WebJobs.Host.TraceWriter log)
+        {
+            this.log = log;
+        }
+
+        public void Debug(string message)
+        {
+            this.log.Verbose(message);
+        }
+
+        public void Info(string message)
+        {
+            this.log.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            this.log.Warning(message);
+        }
+
+        public void Error(string message)
+        {
+            this.log.Error(message);
+        }
+
+        public void Error(Exception ex, string message)
+        {
+            this.log.Error(message, ex);
+        }
+    }
+}
