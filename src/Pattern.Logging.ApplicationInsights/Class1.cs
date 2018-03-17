87237ed62f7bda73bede2fd72dcf42c9ec4b7059
Aurélien GALTIER <aurelien.galtier@live.fr>
2018-03-17 13:37:25 +0100

    Add begin of appinsights

---
+++ src/Pattern.Logging.ApplicationInsights/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+
+namespace Pattern.Logging.ApplicationInsights
+{
+    public class ApplicationInsights : ILogger
+    {
+        private TelemetryClient telemetryClient;
+
+        public ApplicationInsights()
+        {
+            telemetryClient = new TelemetryClient();
+        }
+
+        public void Debug(string message)
+        {
+            telemetryClient.TrackException();
+        }
+
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception ex, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
